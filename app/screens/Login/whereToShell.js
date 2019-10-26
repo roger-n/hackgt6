a1d263451ad8dae0ee689fda990fc83154ea483e
Dash <dashcc1999@gmail.com>
2019-10-26 15:46:26 -0400

    message

--- app/screens/Login/whereToShell.js
+++ app/screens/Login/whereToShell.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
-import { TextInput, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet, Button, Alert, Image } from "react-native";
+import SelectMultiple from "react-native-select-multiple";
 
-export default class whereToShell extends Component {
-    
-    constructor () {
-        super()
-        this.state = { result: 'Result of editing should appear here.' }
-      }
+const fruits = ["Starbucks", "Dunkin Donuts", "Carribou", "Gus's Best"];
+export default class whereToShell extends React.Component {
+  state = { selectedFruits: [] };
 
-    render() {
-        return (
-            <View>
-                <Text>{this.state.result}</Text>
-                    <TextInput
-                        onSubmitEditing={({ nativeEvent }) => this.setState({ result: nativeEvent.text })}/>
-            </View>
-            ); 
-    }
- 
+  onSelectionsChange = selectedFruits => {
+    this.setState({ selectedFruits });
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Bean.io</Text>
+        <Text style={styles.paragraph}>Favorite Shops?</Text>
+        <SelectMultiple
+          items={fruits}
+          selectedItems={this.state.selectedFruits}
+          onSelectionsChange={this.onSelectionsChange}
+        />
+        <Image style={styles.logo} source={require("coffee.png")} />
+        <Button
+          title="Where's work?..."
+          onPress={() => {
+            Alert.alert("Simple Button pressed");
+            this.setState({ color: "#000000" });
+          }}
+        />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#853721",
+    padding: 8
+  },
+  title: {
+    margin: 24,
+    fontSize: 36,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white"
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white"
+  },
+  logo: {
+    height: 128,
+    width: 128
+  }
+});
